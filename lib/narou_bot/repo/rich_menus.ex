5409b1d507d879a8439efa61743dcae06b1eebb5
HarukiKubota <ricca545@icloud.com>
2020-06-20 11:49:58 +0900

    add Repositories

---
+++ lib/narou_bot/repo/rich_menus.ex
@@ -0,0 +1,12 @@
+defmodule NarouBot.Repo.RichMenus do
+  import Ecto.Query
+  alias NarouBot.Repo
+  alias NarouBot.Entity.RichMenu
+
+  def all, do: Repo.all(RichMenu)
+  def find_by_name(menu_name), do: Repo.get_by(RichMenu, name: menu_name)
+
+  def create(remote_id, name), do: %RichMenu{} |> Map.merge(%{remote_id: remote_id, name: name}) |> Repo.insert!
+
+  def delete_all!, do: from(RichMenu) |> Repo.delete_all()
+end
