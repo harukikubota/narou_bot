9829601c277bcfcf6be698f4a61392879089fb82
HarukiKubota <ricca545@icloud.com>
2020-06-20 11:34:05 +0900

    add db migrations

---
+++ priv/repo/migrations/20200517192516_create_rich_menu.exs
@@ -0,0 +1,10 @@
+defmodule NarouBot.Repo.Migrations.CreateRichMenu do
+  use Ecto.Migration
+
+  def change do
+    create table(:rich_menus) do
+      add :remote_id, :string, null: false
+      add :name,      :string, null: false
+    end
+  end
+end
