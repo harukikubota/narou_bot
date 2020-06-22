14e007937e65145ad31272ab77398816420b3d4e
HarukiKubota <ricca545@icloud.com>
2020-06-22 23:33:30 +0900

    add Help 中身は今度書く

--- lib/narou_bot/command/utility/help.ex
+++ lib/narou_bot/command/utility/help.ex
@@ -2,6 +2,6 @@ defmodule NarouBot.Command.Utility.Help do
   use NarouBot.Command
 
   def call(param) do
-
+    render_with_send(:ok, nil, param.key)
   end
 end
--- lib/narou_bot/template/utility/help.ex
+++ lib/narou_bot/template/utility/help.ex
@@ -2,7 +2,7 @@ defmodule NarouBot.Template.Utility.Help do
   use NarouBot.Template
   alias LineBot.Message, as: M
 
-  def render(:ok, dao) do
-
+  def render(:ok, _) do
+    %M.Text{text: "ヘルプは未実装です。しばしお待ちを。。。"}
   end
 end
