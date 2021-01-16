c72edae69fa8f8146ba4f64bc586b158a6d0519c
HarukiKubota <ricca545@icloud.com>
2021-01-16 15:44:18 +0900

    refactor: Command

--- lib/narou_bot.ex
+++ lib/narou_bot.ex
@@ -6,4 +6,17 @@ defmodule NarouBot do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def command do
+    quote do
+      import NarouBot.Command.Helper
+    end
+  end
+
+  @doc """
+  When used, dispatch to the appropriate command/template/etc.
+  """
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
 end
--- lib/narou_bot/command.ex
+++ lib/narou_bot/command.ex
@@ -1,30 +1,43 @@
 defmodule NarouBot.Command do
   defmacro __using__(_) do
     quote do
+      use NarouBot, :command
+
       alias LineBot.Message
       alias NarouBot.Command.MessageServer, as: MS
-      alias NarouBot.Command.Helper
 
-      def init(token),          do: {:ok, pid} = MS.start_link(token)
-      def template(),           do: NarouBot.Command.template_name(__MODULE__)
-      def messages(pid),        do: MS.get_messages(pid)
-      def reply_token(pid),     do: MS.get_reply_token(pid)
-      def render(id, dao, pid), do: template().render(id, dao) |> MS.push(pid)
-      def send(pid),            do: LineBot.send_reply(reply_token(pid), messages(pid))
-      def render_with_send(id, dao, pid) do
-        render(id, dao, pid)
-        send(pid)
+      defmacrop template do
+        quote do
+          to_string(__MODULE__)
+          |> String.split(".")
+          |> List.replace_at(2, "Template")
+          |> Enum.join(".")
+          |> String.to_atom
+        end
       end
-      def close(pid), do: MS.stop(pid)
-    end
-  end
 
-  def template_name(module) do
-    module
-    |> to_string
-    |> String.split(".")
-    |> List.replace_at(2, "Template")
-    |> Enum.join(".")
-    |> String.to_atom
+      defmacrop key do
+        quote do: self()
+      end
+
+      def init(reply_token),   do: MS.start_link(key(),reply_token)
+      def setup(param),        do: param
+      def callable?(_param),   do: true
+      def call(_param),        do: IO.inspect "override this." # do something
+      def close(),             do: MS.stop(key())
+
+      defoverridable setup: 1, callable?: 1, call: 1
+
+      defp export(vars),        do: Enum.each(vars, fn {k, v} -> MS.push_val(key(), k, v) end)
+      defp render(id),          do: MS.push_message(key(), template().render(id, MS.get_dao(key())))
+      defp send_message(),      do: LineBot.send_reply(reply_token(), messages())
+      defp render_with_send(id) do
+        render(id)
+        send_message()
+      end
+
+      defp messages(),          do: MS.get_messages(key())
+      defp reply_token(),       do: MS.get_reply_token(key())
+    end
   end
 end
